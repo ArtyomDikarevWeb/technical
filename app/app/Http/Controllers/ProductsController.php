f2dfaa118b0571f842ccba488cf4985ef7ad051d
Belho <bellumhon@gmail.com>
2024-02-12 16:51:55 +0400

    (feat): added frontend

--- app/app/Http/Controllers/ProductsController.php
+++ app/app/Http/Controllers/ProductsController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Jobs\AddProductsFromApiJob;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Redis;
 
 class ProductsController extends Controller
@@ -17,22 +20,35 @@ class ProductsController extends Controller
         $this->productJob = new AddProductsFromApiJob($this->jobUniqueId);
     }
 
+    public function index()
+    {
+        if (Redis::exists('user')) {
+            Cookie::queue(Cookie::make('user', Redis::get('user'), httpOnly: false));
+        }
+
+        return view('main');
+    }
+
     public function loadProductsFromApi(): JsonResponse
     {
         Redis::set('user', json_encode(['user_unique_job_id' => $this->jobUniqueId]));
         Redis::set('job_'.$this->jobUniqueId, json_encode(['status' => $this->productJob::ONQUEUE]));
         dispatch($this->productJob);
 
-        return response()->json(['data' => 'success']);
+        return response()->json(['data' => 'success'])
+            ->cookie('user',  Redis::get('user'), 3600, '/', null, true, false);
     }
 
-    public function isJobDone(): JsonResponse
+    public function isJobDone(Request $request): JsonResponse
     {
-        $data = json_decode(Redis::get('user'));
-        $jobData = json_decode(Redis::get('job_'.$data->user_unique_job_id));
+        $cookie = json_decode($request->cookie('user'));
+        $jobData = json_decode(Redis::get('job_'.$cookie->user_unique_job_id));
 
         if ($jobData->status === $this->productJob::DISPATCHED) {
-            return response()->json(['data' => ['status' => $this->productJob::DISPATCHED]], 200);
+            Redis::del('user');
+            return response()
+                ->json(['data' => ['status' => $this->productJob::DISPATCHED]], 200)
+                ->cookie('user', '', 0);
         }
 
         if ($jobData->status === $this->productJob::PENDING) {
@@ -40,7 +56,10 @@ class ProductsController extends Controller
         }
 
         if ($jobData->status === $this->productJob::FAILED) {
-            return response()->json(['data' => ['status' => $this->productJob::FAILED]], 400);
+            Redis::del('user');
+            return response()
+                ->json(['data' => ['status' => $this->productJob::FAILED]], 400)
+                ->cookie('user', '', 0);
         }
 
         if ($jobData->status === $this->productJob::ONQUEUE) {
